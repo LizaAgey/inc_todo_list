fede762ae5433ed73d95c42509bfcc3682230ea6
Liza Agey <agey.eliz@gmail.com>
2022-11-15 16:02:56 +0700

    first commit

---
+++ src/TodoList.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {TaskType} from './App';
+
+//1.Variable
+//2.Parm of fun
+//3.Retunr of func
+
+type TodoListPropsType = {
+    title: string
+    tasks: Array<TaskType>
+}
+
+const TodoList = (props: TodoListPropsType) => {
+
+
+    const tasksListItem = props.tasks.map((task: TaskType) => {
+        return (
+
+                <li key={task.id}>
+                    <input type="checkbox" checked={task.isDone}/>
+                    <span>{task.title}</span>
+                    <button onClick={() => alert(task.id)}>x</button>
+                </li>
+
+        )
+
+    })
+
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {tasksListItem}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+};
+
+export default TodoList;
